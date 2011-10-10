abe5fcfd88b72e710d6347feeead1476671b0eab
Muhammad Rafii Pratama <muhammadrafiipratama@rocketmail.com>
2011-10-10 12:42:55 +0700

    soal1

---
+++ sesi4/src/soal1/Praktikum.java
@@ -0,0 +1,28 @@
+public class Praktikum{
+			public static void main(String args[]){
+			//inisialisasi variabel number1, number2 results dengan tipe data integer
+			int number1, number2, results;
+			//inisialisasi variabel number dengan tipe data boolean
+			boolean number;
+			//meber nilai 20 pada variabel number1
+			number1 = 20;
+			////meber nilai 20 pada variabel number2
+			number2 = 10;
+		//menghitung  number1 / number2 dan hasilnya disimpan oleh variasi results	
+		results = number1 / number2;
+		//mencetak keluaran hasil pembagian variabel results
+		System.out.println(results);
+		//menghitung  number1 + number2 dan hasilnya disimpan oleh variasi results		
+		results = number1 + number2;
+		//mencetak keluaran hasil pembagian variabel results
+		System.out.println(results);
+		//mencocokkan apakah 20 tidak sama dengan 10 dan hasilnya akan disimpan oleh varial number
+		number = (20 != 10);
+		//mencetak keluaran hasil varibel number
+		System.out.println(results);
+		//mencocokkan apakah 20 tidak sama dengan 10 dan hasilnya akan disimpan oleh varial number
+		number = (20 == 10);
+		//mencetak keluaran hasil varibel number
+		System.out.println(results);
+	}
+}
