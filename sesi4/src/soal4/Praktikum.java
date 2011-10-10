049f65fcbd9761dbc741713385bd041547bef209
Muhammad Rafii Pratama <muhammadrafiipratama@rocketmail.com>
2011-10-10 12:44:11 +0700

    soal4

---
+++ sesi4/src/soal4/Praktikum.java
@@ -0,0 +1,13 @@
+public class Praktikum{
+	public static void main(String[] args){
+	//mencetak keluaran word "Instr 1"
+		System.out.println("Instr 1");
+		//jika 3 lebih besar dari 2 maka mencetak keluaran word "Instr 2"
+		if(3>2){
+			System.out.println("Instr 2");
+		}else{
+			System.out.println("Instr 3");
+		}
+		System.out.println("Instr 4");
+	}
+}
