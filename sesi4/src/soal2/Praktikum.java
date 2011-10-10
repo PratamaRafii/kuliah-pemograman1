bf58ab9b3eb9dd30a64ec2d5d587381c22e16bd8
Muhammad Rafii Pratama <muhammadrafiipratama@rocketmail.com>
2011-10-10 12:43:16 +0700

    soal2

---
+++ sesi4/src/soal2/Praktikum.java
@@ -0,0 +1,12 @@
+public class Praktikum{
+		public static void main(String args[]){
+		//melakukan perlopingan dengan for
+		//inisialisasi variabel x dengan tipe integer
+		//member nilai 0 pada variabel x
+		//jika x lebih kecil dari 7
+		for (int x = 0; x < 7; x++){
+		//maka mencetak keluaran berupa  words "Hello, Worlds we are using loops"
+				System.out.println("Hello, Worlds we are using loops");
+			}
+		}
+}
