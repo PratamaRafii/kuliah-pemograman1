cdf6f0847b79b6fd9c9403ae8b59af957b2ea966
Muhammad Rafii Pratama <muhammadrafiipratama@rocketmail.com>
2011-10-10 12:43:47 +0700

    soal3

---
+++ sesi4/src/soal3/Praktikum.java
@@ -0,0 +1,9 @@
+public class Praktikum{
+		public static void main(String args[]){
+		//melakukan control  flow while denhagn isi tipe  data boolean true 
+		while(true) {
+		//mencetak keluaran berupa words "Hello, Worlds we are using loops"
+				System.out.println("Hello, Worlds we are using loops");
+			}
+		}
+}
