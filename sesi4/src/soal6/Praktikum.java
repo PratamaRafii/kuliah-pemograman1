65b347ad811396a538f70699cc82b5cb4aa4d2b4
Muhammad Rafii Pratama <muhammadrafiipratama@rocketmail.com>
2011-10-10 12:44:57 +0700

    soal6

---
+++ sesi4/src/soal6/Praktikum.java
@@ -0,0 +1,10 @@
+public class Praktikum{
+		public static void main(String args[]){
+			boolean coba1;
+			
+		coba1 = (10 == 1);
+			System.out.println(coba1);
+		coba1 = (10 != 1);
+			System.out.println(coba1);				
+	}
+}
