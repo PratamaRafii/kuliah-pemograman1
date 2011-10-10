5ec3c8ecf8de89761e4509392d7f95e7de751e45
Muhammad Rafii Pratama <muhammadrafiipratama@rocketmail.com>
2011-10-10 12:44:38 +0700

    soal5

---
+++ sesi4/src/soal5/Praktikum.java
@@ -0,0 +1,9 @@
+public class Praktikum{
+		public static void main(String args[]){
+		//inisialisasi variabel a dan memiliki nilai 5
+		//test=5+1
+			int test = 5;
+			System.out.println(test++);
+			System.out.println(test);
+		}
+}
